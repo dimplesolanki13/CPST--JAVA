9623a27020767d65bff70479b8afb2c27ced2ebd
dimplesolanki13 <dimplechoudhary1308@gmail.com>
2026-07-17 14:48:12 +0000

    file

---
+++ CSPT-170726/Prog2.java
@@ -0,0 +1,11 @@
+public class Prog2 {
+    public static void main(String args[]){    
+		int Minval = Integer.MIN_VALUE;
+		System.out.println("minimum value = "+Minval);
+		
+		int Maxval = Integer.MAX_VALUE;
+		System.out.println("maximum value = "+Maxval);
+
+    }
+    
+}

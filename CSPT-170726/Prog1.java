052aa014aa2daef64a4893c74ac88259fd9d3b0b
dimplesolanki13 <dimplechoudhary1308@gmail.com>
2026-07-17 14:32:06 +0000

    file

---
+++ CSPT-170726/Prog1.java
@@ -0,0 +1,9 @@
+void main(){
+    boolean b = true;
+    boolean f = false;
+    IO.println(b+" and "+f);
+    char ch = '3';
+    int y = Integer.parseInt(String.valueOf(ch));
+    IO.println(y);
+
+}

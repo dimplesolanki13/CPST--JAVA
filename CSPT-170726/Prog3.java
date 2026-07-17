af682e669b15c74b17dad81309ff229b76489d6e
dimplesolanki13 <dimplechoudhary1308@gmail.com>
2026-07-17 15:19:42 +0000

    file

---
+++ CSPT-170726/Prog3.java
@@ -0,0 +1,13 @@
+public class Prog3 {
+    public static void main(String args[]){
+        
+        String x = "1A";
+
+        int y = Integer.parseInt(x,16);
+        System.out.println("hexadecimal value is:"+y);
+
+
+    }
+
+    
+}
---
+++ CSPT-170726/prog4.java
@@ -0,0 +1,10 @@
+public class prog4 {
+    public static void main(String agrs[]){
+
+    String x = "32";
+
+    int y = Integer.parseInt(x,8);
+    System.out.println("octadecimal value is:"+y);
+
+  }
+}
